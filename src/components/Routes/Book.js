4985ada2dfdec87567e296b78b01073d1a7c4e8c
Ksigei <sigeikiprono4@gmail.com>
2022-05-17 14:31:17 +0300

    Add redux to the project

---
+++ src/components/Routes/Book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Book() {
+  return (
+    <div>
+      <div className="container">
+        <h3>Title: Title</h3>
+        <h3>Author: Author</h3>
+        <p>
+          The Bookstore is a website similar to the &quote;Awesome Books&quote;
+          website built in the previous module. You will create an MVP version
+          of it that allows you to:
+        </p>
+        <button type="button">Remove book</button>
+      </div>
+    </div>
+  );
+}
+
+export default Book;
